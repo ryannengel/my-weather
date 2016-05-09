a89441282ad9dbe902b5453f40360d12b8aa60b7
ryannengel <engelryann@gmail.com>
2016-05-09 16:11:22 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// My Scripts
+
+
+$(function() {
+  $.simpleWeather({
+    location: 'Spokane, WA',
+    unit: 'f',
+    success: function(weather) {
+      
+//console.log(weather);
+        
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('.cond-code').text(weather.code);
+      
+    
+    },
+    error: function(error) {
+      
+      console.log('Error No Weather');
+      
+    }
+  });
+});
