e0bf2b68f632889c02c28ffe4c9e4449686f53c3
ryannengel <engelryann@gmail.com>
2016-05-09 16:36:50 -0700

    js

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,12 @@ $(function() {
     unit: 'f',
     success: function(weather) {
       
-//console.log(weather);
-        
+      console.log(weather);
+      
       $('.city').text(weather.city);
       $('.temp').text(weather.temp);
       $('.cond-code').text(weather.code);
+      $('.icon img').attr('src', '');
       
     
     },
